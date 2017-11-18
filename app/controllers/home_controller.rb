6a9fec877268ca3e852e7d84a764cb0aece7183d
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-11-17 23:33:24 -0800

    Make terminal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,57 @@
+class Line
+    def initialize(text, isListItem)
+        @text = text
+        @isListItem = isListItem
+    end
+
+    def text
+        @text
+    end
+
+    def isListItem
+        @isListItem
+    end
+end
+
+class SplitLine
+    def initialize(sections, isListItem)
+        @sections = sections
+        @isListItem = isListItem
+    end
+
+    def sections
+        @sections
+    end
+
+    def isListItem
+        @isListItem
+    end
+end
+
 class HomeController < ApplicationController
     helper_method :style
-    LINE_WIDTH = 85
-    SECTION_WIDTH = 5
+    LINE_WIDTH = 60
+    SECTION_WIDTH = 1
+    LINE_COUNT = 10
 
     def show
         lines = [
-            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            "Jacob_Brunsting",
-            "Hosssme", 
-            "about_me"]
-        @lines = Array.new
+            Line.new("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", false),
+            Line.new("Works at some place with some stuff and things", false),
+            Line.new("> Jacob Brunsting", true),
+            Line.new("> Hosssme", true), 
+            Line.new("> about_me", true),
+            Line.new("> Projects", true),
+            Line.new("Workse place with stuff and things", false),
+            Line.new("Works at some with stuff and things", false),
+            Line.new("Also doese at some place with some stuff and things", false),
+            Line.new("Works at some place with some stuff and things", false),
+        ]
+        @splitLines = Array.new
         lines.each do |line|
             splitLine = Array.new
             section = ""
-            line.each_char do |c|
+            line.text.each_char do |c|
                 section = section + c
                 if section.length >= SECTION_WIDTH
                     splitLine.push(section)
@@ -23,39 +61,34 @@ class HomeController < ApplicationController
             if section.length > 0
                 splitLine.push(section)
             end
-            @lines.push(splitLine)
+            @splitLines.push(SplitLine.new(splitLine, line.isListItem))
         end
         render template: "home/home"
     end
 
-    def style(sectionIndex)
+    def style(lineNum, sectionNum)
         sectionCount = LINE_WIDTH / SECTION_WIDTH
-        distFromCenter = sectionCount / 2 - sectionIndex
+        distFromCenterX = sectionCount / 2 - sectionNum
+        distFromCenterY = LINE_COUNT / 2 - lineNum
 
-        transformOrigin = "right"
-        if distFromCenter < 0
-            transformOrigin = "left"
+        scaleX = 1
+        if distFromCenterX.abs > 8
+            scaleX = 1.0 - ((distFromCenterX.abs * distFromCenterX.abs - 8.0) / 3000.0)
         end
+        scaleX *= 1.05
+        translateY = distFromCenterX.abs * distFromCenterX.abs * distFromCenterY * 0.0002
+        rotate = translateY * 5
+        if distFromCenterX > 0
+            rotate *= -1
+        end
+        transformSection = "transform: scaleX(%f) translateY(%fvw) rotate(%ddeg)" % [scaleX, translateY, rotate]
 
-        scaleX = 1.0
-        if distFromCenter.abs > 4
-            scaleX = 1.0 - ((distFromCenter.abs - 4.0) / 8.0)
-        end
+        widthSection = ""
+        width = scaleX * 1
+        widthSection = "width: %fvw" % width
         
-        # wow, a switch case of magic numbers with more magic inside!
-        translateX = 0.0
-        case distFromCenter.abs
-        when 8
-            translateX = 5.2
-        when 7
-            translateX = 2.1
-        when 6
-            translateX = 0.6
-        end
-        if distFromCenter < 0
-            translateX *= -1
-        end
-
-        "transform: scaleX(%f) translateX(%fvw); transform-origin: %s" % [scaleX, translateX, transformOrigin]
+        transformSection + "; " + widthSection
     end
 end
+
+# go to code pen bYYXyQ, look for 'scanlines' ::before
