ed38040ee4c54ede0fb31e9cb82e654ac203b4bb
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-01-07 14:59:05 -0500

    Add content to new home page

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,17 +1,20 @@
 class Post
-    def initialize(thumbnail, title, kind, date, href, image, text)
+    def initialize(thumbnail, title, kind, languages, date, href, image, text)
         @thumbnail = thumbnail
         @title = title
         @kind = kind
+        @languages = languages
         @date = date
         @href = href
         @image = image
         @text = text
+        puts @text
     end
 
     attr_accessor :thumbnail
     attr_accessor :title
     attr_accessor :kind
+    attr_accessor :languages
     attr_accessor :date
     attr_accessor :href
     attr_accessor :image
@@ -20,11 +23,23 @@ end
 
 class FeedController < ApplicationController
     def show
+        jsonAbout = JSON.parse(File.read(Rails.root + "app/assets/javascripts/about.json"))
+        jsonPosts = JSON.parse(File.read(Rails.root + "app/assets/javascripts/contents.json"))
+
+        @about = Post.new("", jsonAbout["title"], "", "", "", "", "", jsonAbout["text"])
         @posts = Array.new
 
-        jsonPosts = JSON.parse(File.read(Rails.root + "app/assets/javascripts/contents.json"))
         jsonPosts.each do |jsonPost|
-            @posts.push(OpenStruct.new(jsonPost))
+            @posts.push(Post.new(
+                jsonPost["thumbnail"], 
+                jsonPost["title"],
+                jsonPost["kind"],
+                jsonPost["languages"],
+                jsonPost["date"],
+                jsonPost["href"],
+                jsonPost["image"],
+                jsonPost["text"]
+            ))
         end
 
         render template: "feed/feed"
